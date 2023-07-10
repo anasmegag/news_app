4bb68b57e2a56415dddfed88f0c37ad751e0030b
anasmegag <megaganas53@gmail.com>
2023-07-10 16:17:52 +0100

    xx

--- lib/view/pages/Article_details.dart
+++ lib/view/pages/Article_details.dart
@@ -36,6 +36,9 @@ class Details extends StatelessWidget {
               ),
               const SizedBox(
                 height: 30,
+
+
+                
               ),
               Text(
                 article.description,
